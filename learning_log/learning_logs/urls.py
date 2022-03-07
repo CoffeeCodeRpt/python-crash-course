762645a20aad4c3dd9e1b17cbd6873128e5717ae
CoffeeCodeRpt <coffeeofficial@protonmail.com>
2022-03-07 00:12:26 -0700

    Update site

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -1,5 +1,6 @@
 """Defines URL patterns for learning_logs."""
 
+from unicodedata import name
 from django.urls import path
 from . import views
 
@@ -9,5 +10,11 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     # Show all topics.
-    path('topics/', views.topics, name='topics')
+    path('topics/', views.topics, name='topics'),
+
+    # Detail page for a single topic
+    path('topic/<int:topic_id>/', views.topic, name='topic'),
+
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
